d98d8425af9a77d0d726bd6e2181b57b427c1ed0
Jonathan Lindqvist <jonathan.lindqvist@elev.ga.ntig.se>
2019-02-26 09:27:54 +0100

    Indexpage (#8)
    * started creating the index page with mdbootstrap
    
    * More bootstrap fixin
    
    * Something wrong with bootstrap, started user pages
    
    * magic mike fixed my footer once again
    
    * Indexpage layout is done, erb in future

---
+++ test/system/learns_test.rb
@@ -0,0 +1,43 @@
+require "application_system_test_case"
+
+class LearnsTest < ApplicationSystemTestCase
+  setup do
+    @learn = learns(:one)
+  end
+
+  test "visiting the index" do
+    visit learns_url
+    assert_selector "h1", text: "Learns"
+  end
+
+  test "creating a Learn" do
+    visit learns_url
+    click_on "New Learn"
+
+    fill_in "Wow", with: @learn.wow
+    click_on "Create Learn"
+
+    assert_text "Learn was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Learn" do
+    visit learns_url
+    click_on "Edit", match: :first
+
+    fill_in "Wow", with: @learn.wow
+    click_on "Update Learn"
+
+    assert_text "Learn was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Learn" do
+    visit learns_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Learn was successfully destroyed"
+  end
+end
