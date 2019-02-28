903feed2e9b96d3ee3e42c52e639eaeacb418081
Jonathan Lindqvist <jonathan.lindqvist@elev.ga.ntig.se>
2019-02-28 14:26:02 +0100

    Backend for user signup done

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   root 'start#index'
 
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
