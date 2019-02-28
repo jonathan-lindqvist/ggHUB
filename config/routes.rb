9ffb8af3ae3f4831d2b20edf1f171876342c862c
Jonathan Lindqvist <47867973+QvistJ@users.noreply.github.com>
2019-02-28 18:01:50 +0100

    2 register (#11)
    * Make a basic User model (including secure passwords)
    
    * Backend for user signup done
    
    * Use SSL and the Puma webserver in production

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
+  get 'users/new'
   resources :learns
   # get 'welcome/index'
   # root 'welcome#index'
   
   get 'start/index'
   root 'start#index'
+
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
