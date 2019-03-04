7897f9472e82338f8912f1fe32b8a1e8c013d74e
Jonathan Lindqvist <47867973+QvistJ@users.noreply.github.com>
2019-03-04 12:03:12 +0100

    3 login (#17)
    * Added basic login/logout functionality
    
    * Advanced login system working
    
    * Advanced login system working

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,35 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  # TODO: Figure what '?' does. https://www.railstutorial.org/book/advanced_login#code-branch_raise
+  # def create 
+  #   ?user = User.find_by(email: params[:session][:email].downcase)
+  #   if ?user && ?user.authenticate(params[:session][:password])
+  #     log_in ?user
+  #     params[:session][:remember_me] == '1' ? remember(?user) : forget(?user)
+  #     redirect_to ?user
+  #   else
+  #     flash.now[:danger] = 'Invalid email/password combination'
+  #     render 'new'
+  #   end
+  # end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_url
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  #get 'sessions/new'
+  #get 'users/new'
   resources :learns
   # get 'welcome/index'
   # root 'welcome#index'
@@ -10,5 +11,9 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   resources :users
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
