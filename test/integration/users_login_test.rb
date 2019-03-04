52193c42981df984c8f446c977119a49d4908b33
Jonathan Lindqvist <jonathan.lindqvist@elev.ga.ntig.se>
2019-03-04 10:58:17 +0100

    Advanced login system working

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -48,4 +48,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+
+  # TODO: Get this test to work, FILL_IN is the problem
+  # test "login with remembering" do
+  #   log_in_as(@user, remember_me: '1')
+  #   assert_equal FILL_IN, assigns(:user).FILL_IN
+  # end
+
+  test "login without remembering" do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies[:remember_token]
+  end
 end
