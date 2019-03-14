f2547c214c105c7f3dbcad10470fd3ade99df08e
Jonathan Lindqvist <jonathan.lindqvist@elev.ga.ntig.se>
2019-03-14 19:13:57 +0100

    added images and feed for microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_075552) do
+ActiveRecord::Schema.define(version: 2019_03_14_141400) do
 
   create_table "learns", force: :cascade do |t|
     t.string "wow"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_03_14_075552) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
