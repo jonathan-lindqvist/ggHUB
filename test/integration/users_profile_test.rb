c3ab7aa3b9b471272fd713efc0a315d6a2b783e4
Jonathan Lindqvist <jonathan.lindqvist@elev.ga.ntig.se>
2019-03-14 14:55:13 +0100

    got the test to fucking work

--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -8,9 +8,7 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
   end
 
   test "profile display" do
-    #byebug
-    #get user_path(@user)
-    get "/users/#{@user.id}"
+    get user_path(@user)
     assert_template 'users/show'
     assert_select 'title', full_title(@user.name)
     assert_select 'h1', text: @user.name
