49ac8aee04b6e32b38f35118345ac51a781b850b
Jonathan Lindqvist <47867973+QvistJ@users.noreply.github.com>
2019-03-15 07:53:56 +0100

    36 micropost (#40)
    * Dev into master (#35)
    
    * Created index page
    
    * Added bootsrap
    
    * Fixed sticky footer (#5)
    
    * Set theme jekyll-theme-slate
    
    * Update issue templates
    
    * Set theme jekyll-theme-cayman
    
    * updted readme and gitignore
    
    * Fixed a sticky footer that now will stay on the bottom, no matter
    how long the content is.
    
    * Indexpage (#8)
    
    * started creating the index page with mdbootstrap
    
    * More bootstrap fixin
    
    * Something wrong with bootstrap, started user pages
    
    * magic mike fixed my footer once again
    
    * Indexpage layout is done, erb in future
    
    * 2 register (#11)
    
    * Make a basic User model (including secure passwords)
    
    * Backend for user signup done
    
    * Use SSL and the Puma webserver in production
    
    * 3 login (#17)
    
    * Added basic login/logout functionality
    
    * Advanced login system working
    
    * Advanced login system working
    
    * 6 profile page (#21)
    
    * Set theme jekyll-theme-slate
    
    * Update issue templates
    
    * Set theme jekyll-theme-cayman
    
    * updted readme and gitignore
    
    * Create CODE_OF_CONDUCT.md (#15)
    
    * Added authorization for edit/update
    
    * added pageination
    
    * CRUD for users complete
    
    * 23 account activation (#27)
    
    * Set theme jekyll-theme-slate
    
    * Update issue templates
    
    * Set theme jekyll-theme-cayman
    
    * updted readme and gitignore
    
    * Create CODE_OF_CONDUCT.md (#15)
    
    * added the mailer and db setup for account activation
    
    * Only activated users will be shown
    
    * 23 account activation (#28)
    
    * Set theme jekyll-theme-slate
    
    * Update issue templates
    
    * Set theme jekyll-theme-cayman
    
    * updted readme and gitignore
    
    * Create CODE_OF_CONDUCT.md (#15)
    
    * added the mailer and db setup for account activation
    
    * Only activated users will be shown
    
    * added sendgrid for production
    
    * 23 account activation (#29)
    
    * Set theme jekyll-theme-slate
    
    * Update issue templates
    
    * Set theme jekyll-theme-cayman
    
    * updted readme and gitignore
    
    * Create CODE_OF_CONDUCT.md (#15)
    
    * added the mailer and db setup for account activation
    
    * Only activated users will be shown
    
    * added sendgrid for production
    
    * changed mail
    
    * 23 account activation (#34)
    
    * Set theme jekyll-theme-slate
    
    * Update issue templates
    
    * Set theme jekyll-theme-cayman
    
    * updted readme and gitignore
    
    * Create CODE_OF_CONDUCT.md (#15)
    
    * added the mailer and db setup for account activation
    
    * Only activated users will be shown
    
    * added sendgrid for production
    
    * changed mail
    
    * 30 password reset (#33)
    
    * Set theme jekyll-theme-slate
    
    * Update issue templates
    
    * Set theme jekyll-theme-cayman
    
    * updted readme and gitignore
    
    * Create CODE_OF_CONDUCT.md (#15)
    
    * First part of password reset done
    
    * Password reset done
    
    * basic parts complete for microposts
    
    * fuck ths version
    
    * got the test to fucking work
    
    * added images and feed for microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_08_085223) do
+ActiveRecord::Schema.define(version: 2019_03_14_141400) do
 
   create_table "learns", force: :cascade do |t|
     t.string "wow"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "picture"
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
