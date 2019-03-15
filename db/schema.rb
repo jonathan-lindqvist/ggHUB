aa7e8744a3eeffb1c53da7170cfcd4e7f9f122b2
Jonathan Lindqvist <47867973+QvistJ@users.noreply.github.com>
2019-03-15 10:34:30 +0100

    37 following (#43)
    * Dev into master (#35)
    
    * Created index page
    
    * Added bootsrap
    
    * Fixed sticky footer (#5)
    
    * Set theme jekyll-theme-slate
    
    * Update issue templates
    
    * Set theme jekyll-theme-cayman
    
    * updted readme and gitignore
    
    * Fixed a sticky footer that now will stay on the bottom, no matter
    how long the content is.
    
    * Indexpage (#8)
    
    * started creating the index page with mdbootstrap
    
    * More bootstrap fixin
    
    * Something wrong with bootstrap, started user pages
    
    * magic mike fixed my footer once again
    
    * Indexpage layout is done, erb in future
    
    * 2 register (#11)
    
    * Make a basic User model (including secure passwords)
    
    * Backend for user signup done
    
    * Use SSL and the Puma webserver in production
    
    * 3 login (#17)
    
    * Added basic login/logout functionality
    
    * Advanced login system working
    
    * Advanced login system working
    
    * 6 profile page (#21)
    
    * Set theme jekyll-theme-slate
    
    * Update issue templates
    
    * Set theme jekyll-theme-cayman
    
    * updted readme and gitignore
    
    * Create CODE_OF_CONDUCT.md (#15)
    
    * Added authorization for edit/update
    
    * added pageination
    
    * CRUD for users complete
    
    * 23 account activation (#27)
    
    * Set theme jekyll-theme-slate
    
    * Update issue templates
    
    * Set theme jekyll-theme-cayman
    
    * updted readme and gitignore
    
    * Create CODE_OF_CONDUCT.md (#15)
    
    * added the mailer and db setup for account activation
    
    * Only activated users will be shown
    
    * 23 account activation (#28)
    
    * Set theme jekyll-theme-slate
    
    * Update issue templates
    
    * Set theme jekyll-theme-cayman
    
    * updted readme and gitignore
    
    * Create CODE_OF_CONDUCT.md (#15)
    
    * added the mailer and db setup for account activation
    
    * Only activated users will be shown
    
    * added sendgrid for production
    
    * 23 account activation (#29)
    
    * Set theme jekyll-theme-slate
    
    * Update issue templates
    
    * Set theme jekyll-theme-cayman
    
    * updted readme and gitignore
    
    * Create CODE_OF_CONDUCT.md (#15)
    
    * added the mailer and db setup for account activation
    
    * Only activated users will be shown
    
    * added sendgrid for production
    
    * changed mail
    
    * 23 account activation (#34)
    
    * Set theme jekyll-theme-slate
    
    * Update issue templates
    
    * Set theme jekyll-theme-cayman
    
    * updted readme and gitignore
    
    * Create CODE_OF_CONDUCT.md (#15)
    
    * added the mailer and db setup for account activation
    
    * Only activated users will be shown
    
    * added sendgrid for production
    
    * changed mail
    
    * 30 password reset (#33)
    
    * Set theme jekyll-theme-slate
    
    * Update issue templates
    
    * Set theme jekyll-theme-cayman
    
    * updted readme and gitignore
    
    * Create CODE_OF_CONDUCT.md (#15)
    
    * First part of password reset done
    
    * Password reset done
    
    * updated git ignore
    
    * dunno thb 2.0
    
    * added follow/unfollow system

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_14_141400) do
+ActiveRecord::Schema.define(version: 2019_03_15_081357) do
 
   create_table "learns", force: :cascade do |t|
     t.string "wow"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 2019_03_14_141400) do
     t.string "picture"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
   create_table "users", force: :cascade do |t|
