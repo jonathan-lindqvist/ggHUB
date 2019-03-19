bd86b71749364f61ec934cb732b579801d5cb057
Jonathan Lindqvist <47867973+QvistJ@users.noreply.github.com>
2019-03-19 09:40:47 +0100

    erb for players is done (#47)

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -1,5 +1,6 @@
 class StartController < ApplicationController
   def index
+    @players = User.order("created_at").last(15)
     if logged_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
