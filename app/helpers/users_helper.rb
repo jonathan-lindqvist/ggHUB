452384b3c4fd3f1f10102e2ed436da8ab8c7470e
Jonathan Lindqvist <jonathan.lindqvist@elev.ga.ntig.se>
2019-03-22 10:35:38 +0100

    Improved css for index, users and feed

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,6 +7,13 @@ module UsersHelper
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+    image_tag(gravatar_url, alt: user.name)
+  end
+
+  def disabled_gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: 'disabled_img')
   end
 end
