3f8352a8cc86d75d1d767a44e20e038d4074d07a
Jonathan Lindqvist <jonathan.lindqvist@elev.ga.ntig.se>
2019-03-25 10:54:36 +0100

    Fixed issue :)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,10 @@ module ApplicationHelper
       page_title + ' | ' + base_title
     end
   end
+
+  def current_class?(test_path)
+    return 'active' if request.path == test_path
+
+    ''
+  end
 end
