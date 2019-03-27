8649944c396d28156c116002b2fccb888a56705b
Jonathan Lindqvist <47867973+QvistJ@users.noreply.github.com>
2019-03-27 09:19:31 +0100

    13 uiroles (#61)
    * Added the ability for admins to change roles for users
    
    * Added tests for role change

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,9 @@ Rails.application.routes.draw do
 
   get '/feed', to: 'microposts#index'
 
+  patch '/roleupdate', to: 'users#roleupdate'
+  put '/roleupdate', to: 'users#roleupdate'
+  
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -17,7 +17,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     first_page_of_users.each do |user|
       if user.activated
         assert_select 'a[href=?]', user_path(user), text: user.name
-        assert_select 'a[href=?]', user_path(user), text: 'delete' unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'Delete' unless user == @admin
       end
     end
     assert_difference 'User.count', -1 do
